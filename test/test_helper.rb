77ecb52c7836c582cd95e872d3147da2eef25d50
tompng <tomoyapenguin@gmail.com>
2023-04-17 20:30:10 +0900

    measure coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
+require 'simplecov'            # These two lines must go first
+SimpleCov.start
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+require 'katakata_irb'
 
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "katakata_irb"
-
-require "minitest/autorun"
+require 'minitest/autorun'
